a0d52265943bb7fb049d35a52333c4473e12dcba
Samuel Fox <samueljasonfox@gmail.com>
2021-12-02 00:52:42 -0800

    enable cors

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ const cors = require('cors')
 const { corsOptions } = require('../config/cors')
 
 
-// router.post('api/email', cors(corsOptions), function (req, res, next) {
-router.post('/email', (req, res, next) => {
+router.post('/email', cors(corsOptions), function (req, res, next) {
     res.json({ msg: 'This is CORS-enabled for an allowed domain.' })
 })
 
